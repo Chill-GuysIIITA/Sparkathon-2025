b1bee36836a6163e6c2f34e584e7b61a0206a5bd
Arpit Anand <invoker.arpit@gmail.com>
2025-07-08 17:20:36 +0530

    Initialized next.js frontend prototype

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-4">Next.js Prototype</h1>
+    </main>
+  );
+}
